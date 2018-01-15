e5b1fa24039f4c289b9cf0fba095ed14e89b9b71
jpkim921 <jpkim921@gmail.com>
2018-01-15 13:37:47 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.unshift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -27,9 +27,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.last
 end
